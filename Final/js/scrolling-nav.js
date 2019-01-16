8f5eb7326894d5eb8da2cfe8cef219f1a694fd25
nasej0428 <45926273+nasej0428@users.noreply.github.com>
2019-01-16 21:36:58 +0900

    Add files via upload

---
+++ Final/js/scrolling-nav.js
@@ -0,0 +1,94 @@
+(function($) {
+  "use strict"; // Start of use strict
+
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function() {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top - 54)
+        }, 1000, "easeInOutExpo");
+        return false;
+      }
+    }
+  });
+
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#mainNav',
+    offset: 54
+  });
+
+})(jQuery); // End of use strict
+
+// JavaScript Document
+
+
+$(document).ready(function() {
+	
+	var current=0;
+	var slide_length = $('.slide_ul>li').length;//이미지의 갯수를 변수로
+	var btn_ul = '<ul class="slide_btn"></ul>';//버튼 LIST 작성할 UL
+	
+
+
+	$('.slide_ul>li').hide();//이미지 안보이게
+	$('.slide_ul>li').first().show();//이미지 하나만 보이게
+	
+	
+	$(btn_ul).prependTo($('.slide'))//slide 클래스위에 생성
+	for (var i = 0 ; i < slide_length; i++){//동그라미 버튼 생성 이미지 li 개수 만큼
+		var child = '<li><a href="#none">'+i+'</a></li>';
+		$(child).appendTo($('.slide_btn'));
+	}
+	
+	$('.slide_btn > li > a').first().addClass('active');	
+	$('.slide_btn > li > a').on('click' , slide_stop);
+	
+//자동 슬라이드 함수
+function autoplay(){
+	
+	if(current == slide_length-1){
+	current = 0;
+}else{
+	current++;
+}
+	$('.slide_ul>li').stop().fadeOut(1000);
+	$('.slide_ul>li').eq(current).stop().fadeIn(1000);
+	$('.slide_btn > li > a').removeClass('active');	
+	$('.slide_btn > li > a').eq(current).addClass('active');	
+}
+setInterval(autoplay,3000);//반복
+
+//버튼 클릭시 호출되는 함수
+function slide_stop(){
+		var fade_idx = $(this).parent().index(); 
+		current = $(this).parent().index();//클릭한 버튼의 Index 를 받아서 그 다음 이미지부터 슬라이드 재생.
+		if($('.slide_ul > li:animated').length >= 1) return false; //버튼 반복 클릭시 딜레이 방지
+		$('.slide_ul > li').fadeOut(400);
+		$('.slide_ul > li').eq(fade_idx).fadeIn(400);
+		$('.slide_btn > li > a').removeClass('active');	
+		$(this).addClass('active');
+		
+	}	
+});
+
+/* 더보기 기능 추가 */
+$(function() {
+	$('.more-btn').on('click', function() {
+		if ($(this).children().is('.open')) {
+			$(this).html('<p class="close">CLOSE</p>').addClass('close-btn');
+			$(this).parent().removeClass('slide-up').addClass('slide-down');
+		} else {
+			$(this).html('<p class="open">MORE</p>').removeClass('close-btn');
+			$(this).parent().removeClass('slide-down').addClass('slide-up');
+		}
+	});
+});
